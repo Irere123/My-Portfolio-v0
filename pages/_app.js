04daa986be6de3f9062d98d693380068558a2649
irere123 <irere2050@gmail.com>
2021-08-30 00:23:35 +1400

    feat: added social media meta tags for twitter and facebook

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from "next/head";
 
 import "../styles/globals.css";
 import Header from "../components/Header";
@@ -8,6 +8,26 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" type="img/svg" href="/logo.svg" />
+        <meta property="og:title" content="Hi, I'am Irere Emmanuel" />
+        <meta
+          property="og:description"
+          content="Do you want get full build website that meet your requirements and build with modern technologies. Then I am here for you."
+        />
+        <meta
+          property="og:image"
+          content="https://www.irere.vercel.app/public/irere.jpg"
+        />
+        <meta property="og:url" content="https://irere.vercel.app/" />
+        <meta name="twitter:title" content="Hi, I'am Irere Emmanuel" />
+        <meta
+          name="twitter:description"
+          content="Do you want get full build website that meet your requirements and build with modern technologies. Then I am here for you."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.irere.vercel.app/public/irere.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Header />
       <Component {...pageProps} />
