9011e3ff3536c0872de3ac57a877d734e95c0114
irere123 <irere2020@outlook.com>
2021-09-09 14:51:07 +0200

    fix: fixed social links and a preview image

--- pages/_app.js
+++ pages/_app.js
@@ -11,22 +11,16 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content="Hi, I'am Irere Emmanuel" />
         <meta
           property="og:description"
-          content="Do you want get full build website that meet your requirements and build with modern technologies. Then I am here for you."
+          content="What's up this is my a link to my website where you can get to know whta can do."
         />
-        <meta
-          property="og:image"
-          content="https://www.irere.vercel.app/public/irere.jpg"
-        />
+        <meta property="og:image" content="/irere.jpg" />
         <meta property="og:url" content="https://irere.vercel.app/" />
         <meta name="twitter:title" content="Hi, I'am Irere Emmanuel" />
         <meta
           name="twitter:description"
           content="Do you want get full build website that meet your requirements and build with modern technologies. Then I am here for you."
         />
-        <meta
-          name="twitter:image"
-          content="https://www.irere.vercel.app/public/irere.jpg"
-        />
+        <meta name="twitter:image" content="/irere.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Header />
